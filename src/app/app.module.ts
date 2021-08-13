77833e9d263bf527555bf7e89f323bd5f16429ed
Ricardo301 <ricardo9251@gmail.com>
2021-08-13 16:59:37 -0300

    app module fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
+//import { HeaderComponent } from './shared/header/header.component';
+//import { FooterComponent } from './shared/footer/footer.component';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent
+    //HeaderComponent,
+    //FooterComponent
   ],
   imports: [
     BrowserModule,
